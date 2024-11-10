3691eebd2e4a1d0d7713c17cf989562665bf7453
Utsho11 <utshoroy521@gmail.com>
2024-11-11 00:33:34 +0600

    first commit

---
+++ src/main/java/group2/germentlabtaskwithjava/GermentLabTaskWithJava.java
@@ -0,0 +1,9 @@
+package group2.germentlabtaskwithjava;
+
+
+public class GermentLabTaskWithJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
